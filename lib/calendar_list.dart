e484432643543b5b64020d8d587c86626a10add6
laiiihz <laiiihz@outlook.com>
2020-08-02 15:11:57 +0800

    修复依赖

--- lib/calendar_list.dart
+++ lib/calendar_list.dart
@@ -1,3 +1,11 @@
 library calendar_list;
 
 export 'package:calendar_list/calendar_list.dart';
+export 'package:calendar_list/controller/calendar_list_controller.dart';
+export 'package:calendar_list/models/date_model.dart';
+export 'package:calendar_list/models/month_page_config.dart';
+export 'package:calendar_list/models/performance_config.dart';
+export 'package:calendar_list/utils/calendar_util.dart';
+export 'package:calendar_list/utils/lunar_util.dart';
+export 'package:calendar_list/widgets/calendar_list.dart';
+export 'package:calendar_list/widgets/month_page.dart';
